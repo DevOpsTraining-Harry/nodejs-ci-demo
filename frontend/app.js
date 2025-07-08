be131c31eeff8fff0ba493cb5a0c881448e83460
haarishpp <haarishpp@gmail.com>
2025-07-08 08:54:47 +0530

    Initial commit

---
+++ frontend/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  res.end('Frontend: Hello from Node.js app!\n');
+});
+server.listen(PORT, () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
