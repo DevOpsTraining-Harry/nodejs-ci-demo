d8584165ebbb4976f3991edb4d19b51863191685
haarishpp <haarishpp@gmail.com>
2025-07-10 11:32:40 +0530

    updating app

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-  res.end('Frontend: Hello, update from Node.js app!\n');
+  res.end('Frontend: Hello Sahal, update from Node.js app!\n');
 });
 server.listen(PORT, () => {
   console.log(`Frontend running on port ${PORT}`);
